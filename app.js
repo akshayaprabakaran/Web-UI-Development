bcc0e1c88cfe4161a20df996a310aed436aa00f6
AKSHAYA PRABAKARAN <aksh@AKSHAYAs-MacBook-Air.local>
2020-02-17 10:18:19 -0800

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var lineReader = require('line-reader');
+//var index = require('./app_server/routes/index');
+var app = express();
+
+//View engine setup
+//app.set('views', path.join(__dirname, 'app_server', 'views'));
+//app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app_server/views/jobtrackapp.html'));
+});
+
+
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, '/app_server')));
+//app.use(session( {secret: "String for encrypting cookies." } ));
+
+
+
+//module.exports = app;
+app.listen(3000);
+console.log("Running port 3000");
+
+
---
+++ app_server/views/proj.js
@@ -0,0 +1,110 @@
+    var image = null;
+
+    function loadImageFromLocalStorage() {
+      // If local storage does not have the image, use default
+      if (localStorage.getItem("imgData") === null) {
+        document.getElementById('task').src = "../images/pinterest logo.png";
+        document.getElementById('tsak').src = "../images/walmart logo.png";
+
+      }
+      // If it can find local image, use it
+      else {
+        var dataImage = localStorage.getItem('imgData');
+        document.getElementById('task').src = "data:image/png;base64," + dataImage;
+      }
+    }
+
+    function allowDrop(ev) {
+      ev.preventDefault();
+    }
+
+    function drag(ev) {
+      ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    }
+
+
+  
+
+
+    // Function to save image to Local Storage with a button
+    function changeImage() {
+        bannerImage = document.getElementById('img');
+        imgData = getBase64Image(bannerImage);
+        localStorage.setItem("imgData", imgData);
+    }
+
+    // Store image in local storage
+    function getBase64Image(img) {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        var dataURL = canvas.toDataURL("image/png");
+
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
+
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
