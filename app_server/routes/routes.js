0e35a0c95474dfd31f300f1c71db2fc6ff218c1f
Fuongbregas <fuongbregas@icloud.com>
2020-02-24 03:37:03 -0800

    default page will be register page

--- app_server/routes/routes.js
+++ app_server/routes/routes.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 const controller = require('../controller/main');
 
-router.get('/register', controller.getRegisterPage); // get Register Page
-router.get('/', controller.getHomePage); // get Home Page
+router.get('/home', controller.getHomePage); // get Register
+router.get('/', controller.getRegisterPage); // get Home Page
 router.get('/locate', controller.getLocatePage); // get Locate Page
 router.get('/upload', controller.uploadResume); // get Locate Page
 
