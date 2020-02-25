e49f6499d666b7076dd44c857e0250008b23f9f3
Fuongbregas <fuongbregas@icloud.com>
2020-02-25 02:32:58 -0800

    Added login form

--- app_server/routes/routes.js
+++ app_server/routes/routes.js
@@ -8,5 +8,6 @@ router.get('/home', controller.getHomePage); // get Register
 router.get('/', controller.getRegisterPage); // get Home Page
 router.get('/locate', controller.getLocatePage); // get Locate Page
 router.get('/upload', controller.uploadResume); // get Locate Page
+router.get('/login', controller.getLoginPage); // get Login Page
 
 module.exports = router
