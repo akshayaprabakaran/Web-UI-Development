43a39aaee750dc5ecdf74c6abf4e2ca7d8327d8e
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2020-04-17 14:51:51 -0700

    add controller

--- assignment6-2/server/controller/charts.js
+++ assignment6-2/server/controller/charts.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var JobGrowth = require('./../models/JobGrowth');
-exports.getJobGrowth = (req, res) => {
 
-    
+exports.getJobGrowth = (req, res) => {
+   JobGrowth.find({} ,(err, data) => {
+   		if (err) console.log(err, err.stack); // an error occurred
+   		else res.render('getJobGrowthChart', {jobs: data});
+   		return;
+   })
 }
