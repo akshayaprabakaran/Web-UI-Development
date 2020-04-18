e09a69eecc5c6fb78fed8a00d78398d62fbf011b
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2020-04-18 13:19:55 -0700

    limit women chart data

--- assignment6-2/server/controller/charts.js
+++ assignment6-2/server/controller/charts.js
@@ -4,26 +4,26 @@ var TotalEmp = require('./../models/TotalEmp');
 var Women = require('./../models/Women');
 
 exports.getJobGrowth = (req, res) => {
-   JobGrowth.find({}).limit(19).exec(function(err, data) {
+   JobGrowth.find({}).limit(19).exec(function (err, data) {
       if (err) console.log(err, err.stack); // an error occurred
-		   else res.render('jobGrowthLineChart', {jobs: data});
-		   console.log(data);
-   		return;
+      else res.render('jobGrowthLineChart', { jobs: data });
+      console.log(data);
+      return;
    })
 }
 exports.getTotalEmp = (req, res) => {
-   TotalEmp.find({} ,(err, data) => {
-   		if (err) console.log(err, err.stack); // an error occurred
-		   else res.render('piepie', {percentage: data});
-		   console.log(data);
-   		return;
+   TotalEmp.find({}, (err, data) => {
+      if (err) console.log(err, err.stack); // an error occurred
+      else res.render('piepie', { percentage: data });
+      console.log(data);
+      return;
    })
 }
 exports.getWomen = (req, res) => {
-   Women.find({} ,(err, data) => {
-         if (err) console.log(err, err.stack); // an error occurred
-         else res.render('womenChart', {womenData: data});
-         console.log(data);
-         return;
+   Women.find({}).limit(3).exec(function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else res.render('womenChart', { womenData: data });
+      console.log(data);
+      return;
    })
 }
