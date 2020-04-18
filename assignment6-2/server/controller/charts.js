d82778e896d8e3b9a7c3d10af8d5fcdecb6e3334
Akshaya Prabakaran <43679591+akshayaprabakaran@users.noreply.github.com>
2020-04-18 04:10:54 -0700

    total employment & women startups

--- assignment6-2/server/controller/charts.js
+++ assignment6-2/server/controller/charts.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var JobGrowth = require('./../models/JobGrowth');
+var TotalEmp = require('./../models/TotalEmp');
+var Women = require('./../models/Women');
 
 exports.getJobGrowth = (req, res) => {
    JobGrowth.find({} ,(err, data) => {
@@ -7,5 +9,21 @@ exports.getJobGrowth = (req, res) => {
 		   else res.render('jobGrowthLineChart', {jobs: data});
 		   console.log(data);
    		return;
+   })
+}
+exports.getTotalEmp = (req, res) => {
+   TotalEmp.find({} ,(err, data) => {
+   		if (err) console.log(err, err.stack); // an error occurred
+		   else res.render('piepie', {percentage: data});
+		   console.log(data);
+   		return;
+   })
+}
+exports.getWomen = (req, res) => {
+   Women.find({} ,(err, data) => {
+         if (err) console.log(err, err.stack); // an error occurred
+         else res.render('womenChart', {silicon: data},{san:data},{cal:data});
+         console.log(data);
+         return;
    })
 }
