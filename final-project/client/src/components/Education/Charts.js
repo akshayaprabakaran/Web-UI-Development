075c71aac61d0315061e5b04d8651acda2fe6c46
Akshaya Prabakaran <43679591+akshayaprabakaran@users.noreply.github.com>
2020-04-28 18:38:17 -0700

    education dashboard fix

--- final-project/client/src/components/Education/Charts.js
+++ final-project/client/src/components/Education/Charts.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import axios from 'axios';
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import CanvasJSReact from '../../assets/canvasjs.react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import { MDBContainer } from 'mdbreact';
+//disableScroll.on(); 
+
+
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 Highcharts.setOptions({
-    colors: ["#365253", "#5e9a78","#a7ce9f","#85a47f","#618685"]
+    colors: ["#365253", "#618685","#5e9a78","#85a47f","#a7ce9f"]
 });
 
 class Charts extends Component {
@@ -17,7 +22,7 @@ class Charts extends Component {
             levelData: [],
             gradData:[],
             degreeData:[],
-            stackedColumnChart: {},
+            barChart: {},
             columnChart: {},
             combinationChart: {}
         };
@@ -27,6 +32,7 @@ class Charts extends Component {
         axios
             .get('/dashboard/education')
             .then((res) => {
+                 document.body.style.overflow = 'hidden';
                 let data = res.data;
                 this.setState({ levelData: data.level });
                 this.setState({ gradData: data.grad });
@@ -37,146 +43,105 @@ class Charts extends Component {
             });
     }
 
-    renderStackedColumnChart() {
+    renderBarChart() {
     
-        this.state.stackedColumnChart = {
-            animationEnabled: true,
-                title: {
-                    text: "Educational Attainment(%)",
+        this.state.barChart = {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+               text: "Percentage of Adults by Educational Attainment",
+                    style: {
+            color: "#365253",
                     fontFamily: "Verdana",
-                    fontColor: "#85a47f",
-                    fontSize:25
-                },
-                axisX: {
-                    title: "Location",
-                    crosshair: {
-                        //enabled: true,
-                        snapToDataPoint: true
-                    }
-                },
-                axisY: {
-                    title: "Levels of Education Acheived (%)",
-                    gridThickness:0.5,
-                    gridColor:"#D0D0D0",
-                    crosshair: {
-                        //enabled: true,
-                        snapToDataPoint: true
-                    }
-                },
-                toolTip: {
-                    shared: false,
-                    fontSize:20,
-                    borderThickness:5
-
-                },
-                legend: {
-                    cursor: "pointer",
-                    verticalAlign: "center",
-                    horizontalAlign: "right",
-                    dockInsidePlotArea: false,
-                    fontSize:14,
-                    //itemclick: toogleDataSeries,
-                    reversed:true
-                },
-                data: [
-                {
-                    type: "stackedColumn100",
-                    axisYType: "primary",
-                    name: " < High School",
-                    indexLabelFontSize:15,
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder", 
-                    showInLegend: true,
-                    markerSize: 0,
-                    color:"#a7ce9f",
-                    toolTipContent: "{name}:{y}%",
-                    dataPoints: []
-                },
-                 {
-                    type: "stackedColumn100",
-                    axisYType: "primary",
-                    name: "High School Graduate",
-                    indexLabelFontSize:15,
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder", 
-                    showInLegend: true,
-                    markerSize: 0,
-                    toolTipContent: "{name}:{y}%",
-                    color:"#85a47f",
-                    //lineThickness:2,
-                    dataPoints: []
-                },
-                 {
-                    type: "stackedColumn100",
-                    axisYType: "primary",
-                    name: "College/Associate Degree",
-                    indexLabelFontSize:15,
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder", 
-                    showInLegend: true,
-                    markerSize: 0,
-                    toolTipContent: "{name}:{y}%",
-                    color:"#5e9a78",
-                    //lineThickness:2,
-                    dataPoints: []
-                },
-                {
-                    type: "stackedColumn100",
-                    axisYType: "primary",
-                    name: "Bachelor's Degree",
-                    indexLabelFontSize:15,
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder", 
-                    showInLegend: true,
-                    markerSize: 0,
-                    toolTipContent: "{name}:{y}%",
-                    color:"#618685",
-                    //lineThickness:2,
-                    dataPoints: []
-                },
-                {
-                    type: "stackedColumn100",
-                    axisYType: "primary",
-                    name: "Graduate/Professional Degree",
-                    indexLabelFontSize:15,
-                    indexLabelPlacement: "inside",
-                    indexLabelFontWeight: "bolder", 
-                    showInLegend: true,
-                    markerSize: 0,
-                    toolTipContent: "{name}:{y}%",
-                    color:"#365253",
-                    //lineThickness:2,
-                    dataPoints: []
+                    fontColor: "#365253",
+                    fontSize:23
                 }
-                ]
-        };
-        return Object.keys(this.state.levelData).map((i, index) => {
-            let level = this.state.levelData[i].level;
-            let Less = this.state.levelData[i].Less;
-            let High = this.state.levelData[i].High;
-            let Some = this.state.levelData[i].Some;
-            let Bach = this.state.levelData[i].Bach;
-            let Grad = this.state.levelData[i].Grad;
-            this.state.stackedColumnChart.data[0].dataPoints.push({ label: level, y: Less });
-            this.state.stackedColumnChart.data[1].dataPoints.push({ label: level, y: High });
-            this.state.stackedColumnChart.data[2].dataPoints.push({ label: level, y: Some });
-            this.state.stackedColumnChart.data[3].dataPoints.push({ label: level, y: Bach });
-            this.state.stackedColumnChart.data[4].dataPoints.push({ label: level, y: Grad });
-        });
+            },
+            accessibility: {
+                announceNewData: {
+                    enabled: true
+                },
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+             xAxis:{
+                visible:false
+            },
+            yAxis:{
+                visible:false
+            },
+            legend: {
+                itemStyle: {
+            color: '#000000',
+            fontWeight: 'bold',
+            fontSize:15
+        }
+        },
+            plotOptions: {
+                
+                series: {
+                    pointWidth: 30,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.name}: {point.y:.1f}%'
+                    }
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            },
+            series: [
+            ]
+        }
+       for (var i = 0; i < 5; i++) {
+            this.state.barChart.series.push({ name: '', data: [] });
+        }
 
-    };
+        Object.keys(this.state.levelData).map((i, index) => {
+            let level = this.state.levelData[i].level;
+            let percentage = this.state.levelData[i].percentage;
+            switch (level) {
+                case 'Bachelors Degree':
+                    this.state.barChart.series[0].name = level;
+                    this.state.barChart.series[0].data.push(percentage);
+                    break;
+                case 'Graduate Degree':
+                    this.state.barChart.series[1].name = level;
+                    this.state.barChart.series[1].data.push(percentage);
+                    break;
+                case 'College/Associate Degree':
+                    this.state.barChart.series[2].name = level;
+                    this.state.barChart.series[2].data.push(percentage);
+                    break;
+                case 'High School':
+                    this.state.barChart.series[3].name = level;
+                    this.state.barChart.series[3].data.push(percentage);
+                    break;
+                case 'Less Than HighSchool':
+                    this.state.barChart.series[4].name = level;
+                    this.state.barChart.series[4].data.push(percentage);
+                    break;
+            }
+        });
+    }
 
     renderColumnChart() {
-        var colors = ['#3B97B2', '#67BC42', '#FF56DE', '#E6D605', '#BC36FE', '#000'];
+        //var colors = ['#3B97B2', '#67BC42', '#FF56DE', '#E6D605', '#BC36FE', '#000'];
         this.state.columnChart = {
                 chart:{
                     type:'column'
                 },
                 title: {
-                    text: "Graduation Rate(%)",
+                    text: "Percentage Growth of Graduation Rate",
+                    style: {
+            color: "#365253",
                     fontFamily: "Verdana",
-                    fontColor: "#85a47f",
-                    fontSize:25
+                    fontColor: "#365253",
+                    fontSize:23
+                }
                 },
                 //dataPointWidth: 50,
                 xAxis: {
@@ -190,12 +155,23 @@ class Charts extends Component {
                 yAxis: {
                     min:0,
                     title: {
-                        text:"Percentage"
-                    // gridThickness: 0.3,
-                    // gridColor: "black",
-                    
+                        text:"Percentage",
+                        style:{
+                            fontSize:17,
+                            color:"#000000",
+                            fontFamily: "Verdana"
+
+                        }
+                  
                 }
                 },
+                legend: {
+                itemStyle: {
+            color: '#000000',
+            fontWeight: 'bold',
+            fontSize:15
+        }
+    },
                 toolTip: {
                     headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
@@ -205,67 +181,19 @@ class Charts extends Component {
                 useHTML: true
 
                 },
-                // legend: {
-                //     cursor: "pointer",
-                //     verticalAlign: "bottom",
-                //     horizontalAlign: "center",
-                //     dockInsidePlotArea: false,
-                //     //itemclick: toogleDataSeries,
-                //     fontSize: 15,
-
-                // },
+        
                 plotOptions:{
+                    series: {
+                    pointWidth: 20},
                 column: {
                     pointPadding: 0.2,
                     borderWidth: 0,
-                    //colorByPoint:true
                     }
                 },
                 
                 series:[]
-                // data: [{
-                //     type: "column",
-
-                //     showInLegend: true,
-                //     //color: "rgb(51, 153, 102)",
-                //     axisYType: "primary",
-                //     name: "Graduation Rate",
-                //     showInLegend: true,
-                //     markerSize: 25,
-                //     dataPoints: []
-                // },
-                // {
-                //     type: "column",
-                //     showInLegend: true,
-                //     //color: "rgba(255, 99, 132, 1)",
-                //     axisYType: "primary",
-                //     name: "UC/CSU",
-                //     showInLegend: true,
-                //     markerSize: 10,
-                //     dataPoints: []
-                // },
-                // {
-                //     type: "column",
-                //     showInLegend: true,
-                //     axisYType: "primary",
-                //     //color: "rgb(204, 163, 0)",
-                //     name: "Dropout Rate",
-                //     showInLegend: true,
-                //     markerSize: 10,
-                //     dataPoints: []
-                // }
-                // ]
             }
-    //     return Object.keys(this.state.csvData).map((i, index) => {
-    //         let year = this.state.csvData[i].year;
-    //         let Gradrate = this.state.csvData[i].Gradrate;
-    //         let CSUrate = this.state.csvData[i].CSUrate;
-    //         let Droprate = this.state.csvData[i].Droprate;
-    //         this.state.combinationChart2.data[0].dataPoints.push({ label: year, y: Gradrate });
-    //         this.state.combinationChart2.data[1].dataPoints.push({ label: year, y: CSUrate });
-    //         this.state.combinationChart2.data[2].dataPoints.push({ label: year, y: Droprate });
-    //     });
-    // };
+    
 for (var i = 0; i < 3; i++) {
             this.state.columnChart.series.push({ name: '', data: [] });
         }
@@ -274,15 +202,15 @@ for (var i = 0; i < 3; i++) {
             let rate = this.state.gradData[i].rate;
             let percentages = this.state.gradData[i].percentages;
             switch (rate) {
-                case 'GradRate':
+                case 'Graduation':
                     this.state.columnChart.series[0].name = rate;
                     this.state.columnChart.series[0].data.push(percentages);
                     break;
-                case 'CSURate':
+                case 'CSUandUC':
                     this.state.columnChart.series[1].name = rate;
                     this.state.columnChart.series[1].data.push(percentages);
                     break;
-                case 'DropRate':
+                case 'Dropout':
                     this.state.columnChart.series[2].name = rate;
                     this.state.columnChart.series[2].data.push(percentages);
                     break;
@@ -298,7 +226,7 @@ for (var i = 0; i < 3; i++) {
                 title: {
                     text: "Growth of Science & Engineering Degrees (Yearly)",
                     fontFamily: "Verdana",
-                    fontColor: "#85a47f",
+                    fontColor: "#365253",
                     fontSize:25
                 },
                 axisX: {
@@ -367,29 +295,43 @@ for (var i = 0; i < 3; i++) {
     
 
     render() {
+
+
         return (
+
+            <Container fluid>
+            <div style ={{display: 'flex',paddingBottom: '30px',paddingTop: '10px', justifyContent: 'center',alignItems:'center'}}>
+  <h1>Silicon Valley - Education!</h1>
+</div>
+
+
             <Container>
+            
+
                 {/* Columns are always 50% wide, on mobile and desktop */}
                 <Row>
-                    <Col xs={6}>
-                        {this.renderStackedColumnChart()}
-                        <CanvasJSChart options={this.state.stackedColumnChart} 
+                    <Col xs={7}>
+                        {this.renderBarChart()}
+                        <HighchartsReact
+                            highcharts={Highcharts}
+                            options={this.state.barChart} 
                     />
                     </Col>
-                    <Col xs={6}>
+                    <Col xs={5}  >
                          {this.renderColumnChart()}
                          <HighchartsReact
                             highcharts={Highcharts}
                             options={this.state.columnChart} 
                      />
                      </Col>
-                    <Col xs={12}>
+                    <Col xs={12}style ={{paddingTop: '50px'}} >
                         {this.renderCombinationChart()}
                         <CanvasJSChart options={this.state.combinationChart} 
                     />
                     </Col>
                     
                 </Row>
+            </Container>
             </Container>
         );
     }
