a491dab16e0e21c6e9cd65452700e9dd4bf8b550
fuongbregas <fuongbregas@icloud.com>
2020-04-29 03:01:58 -0700

    Charts

--- final-project/client/src/components/Startups/Chart.js
+++ final-project/client/src/components/Startups/Chart.js
@@ -6,25 +6,106 @@ import CanvasJSReact from '../../assets/canvasjs.react';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Charts extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             earlyStartupData: [],
-            lineChart : {} 
+            lineChart: {}
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('/dashboard/startups')
-        .then((res) => {
-            var data = res.data;
-            this.setState({earlyStartupData: data.earlyStartup});
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+            .then((res) => {
+                var data = res.data;
+                this.setState({ earlyStartupData: data.earlyStartup });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-    
+
+    renderLineChart() {
+        this.state.lineChart = {
+            title: {
+                text: "Number of Seed or Early-Stage Startups, and Total Number of Startup Companies"
+            },
+            axisX: {
+                valueFormatString: "'YY",
+                crosshair: {
+                    enabled: true,
+                    snapToDataPoint: true
+                }
+            },
+            axisY2: {
+                title: "Silicon Valley and San Francisco",
+                crosshair: {
+                    enabled: true,
+                    snapToDataPoint: true
+                }
+            },
+            toolTip: {
+                shared: true
+            },
+            legend: {
+                cursor: "pointer",
+                verticalAlign: "top",
+                horizontalAlign: "center",
+                dockInsidePlotArea: true,
+                itemclick: toogleDataSeries
+            },
+            data: [{
+                type: "line",
+                axisYType: "secondary",
+                name: "Total of Number Startups - San Francisco",
+                showInLegend: true,
+                markerSize: 0,
+                dataPoints: []
+            },
+            {
+                type: "line",
+                axisYType: "secondary",
+                name: "Total of Number Startups - Silicon Valley",
+                showInLegend: true,
+                markerSize: 0,
+                dataPoints: []
+            },
+            {
+                type: "line",
+                axisYType: "secondary",
+                name: "Total of Number Startups - California",
+                showInLegend: true,
+                markerSize: 0,
+                dataPoints: []
+            },
+            {
+                type: "line",
+                axisYType: "secondary",
+                name: "Seed or Early-Stage Startups - San Francisco",
+                showInLegend: true,
+                markerSize: 0,
+                dataPoints: []
+            },
+            {
+                type: "line",
+                axisYType: "secondary",
+                name: "Seed or Early-Stage Startups - Silicon Valley",
+                showInLegend: true,
+                markerSize: 0,
+                dataPoints: []
+            },
+            {
+                type: "line",
+                axisYType: "secondary",
+                name: "Seed or Early-Stage Startups - California",
+                showInLegend: true,
+                markerSize: 0,
+                dataPoints: []
+            }
+            ]
+        };
+        
+    }
 }
 
 export default Charts;
